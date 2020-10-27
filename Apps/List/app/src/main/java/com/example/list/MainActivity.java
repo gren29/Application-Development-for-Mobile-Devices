4178d3bcecb7f1d488b114619e7007bcab61b0c8
RSR <gren2959@gmail.com>
2020-10-27 10:12:24 -0600

    List 2

--- Apps/List/app/src/main/java/com/example/list/MainActivity.java
+++ Apps/List/app/src/main/java/com/example/list/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.list;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends ListActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        ListAdapter la = crearAdapter();
+        setListAdapter(la);
+    }
+
+    protected ListAdapter crearAdapter(){
+        String[] s = new String[] {
+                "Elemento1",
+                "Elemento2",
+                "Elemento3"
+        };
+        ListAdapter la2 = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, s);
+        return la2;
     }
 }
