8165bcbecf512c95ba05ff02821ba12cbc350f42
RSR <gren2959@gmail.com>
2020-10-31 23:32:21 -0600

    SQLite v1

--- Apps/SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
+++ Apps/SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.sqlite;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    EditText    jetI, jetN;
+    Button      jbnA, jbnL;
+    TextView    jtvL;
+    SQLiteDatabase sqld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jetI = (EditText)findViewById(R.id.xetI);
+        jetN = (EditText)findViewById(R.id.xetN);
+        jbnA = (Button)findViewById(R.id.xbnA);
+        jbnL = (Button)findViewById(R.id.xbnL);
+        jtvL = (TextView)findViewById(R.id.xtvL);
+        DbmsSQLiteHelper dsqlh = new DbmsSQLiteHelper(this, "DBContactos", null, 1);
+        sqld = dsqlh.getWritableDatabase();
+        jbnA.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                String id = jetI.getText().toString();
+                String nombre = jetN.getText().toString();
+                ContentValues cv = new ContentValues();
+                cv.put("id", id);
+                cv.put("nombre", nombre);
+                sqld.insert("Contactos", null, cv);
+                jetI.setText(""); jetN.setText("");
+            }
+        });
+
+        jbnL.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                String id, nombre;
+                Cursor c = sqld.rawQuery("SELECT id,nombre FROM Contactos", null);
+                jtvL.setText("");
+                if (c.moveToFirst()) {
+                    do {
+                        id = c.getString(0);
+                        nombre = c.getString(1);
+                        jtvL.append(" " + id + "\t" + nombre + "\n");
+                    }while(c.moveToNext());
+                }
+            }
+        });
     }
 }
