2a47754ef411fdc0dfe9a14bc1db41275d056423
RSR <gren2959@gmail.com>
2020-11-18 16:54:49 -0600

    Canvas

--- Apps/Canvas/app/src/main/java/com/example/canvas/Lienzo.java
+++ Apps/Canvas/app/src/main/java/com/example/canvas/Lienzo.java
@@ -18,18 +18,23 @@ public class Lienzo extends View{
         p.setColor(Color.WHITE);
         c.drawPaint(p);
         p.setColor(Color.BLACK);
-        p.setTextSize(20);
+        p.setTextSize(40);
         c.drawText("x0=" + x/2 + ", y0=" + y/2, x/2 + 20, y/2-20, p);
         p.setColor(Color.rgb(0, 0, 255)); // Ejes azules
         c.drawLine(x/2, 0, x/2, y, p);
         c.drawLine(0, y/2, x, y/2, p);
 
-        p.setTextAlign(Align.RIGHT);
+        p.setTextAlign(Align.LEFT);
         p.setTypeface(Typeface.SERIF);
         c.drawText("Eje X", x-5, y/2-10, p);
-        p.setTextAlign(Align.CENTER); p.setTypeface(Typeface.DEFAULT_BOLD); c.drawText("Eje Y", x/2+30, 20, p);
+        p.setTextAlign(Align.LEFT);
+        p.setTypeface(Typeface.SANS_SERIF); c.drawText("Eje Y", x/2+30, 20, p);
 
         p.setColor(Color.argb(100, 200, 100, 100));
-        c.drawCircle(x/2-120, y/2+120, 200, p);
+        c.drawCircle(x/2-120, y/2+120, 100, p);
+        c.drawOval(100,100,700,400,p);
+        c.drawRect(100,500,300,800,p);
+        c.drawRoundRect(400,500,600,800,20,20,p);
+        c.drawArc( 100,1500,700,2000,0F, -90F, true,p);
     }
 }
