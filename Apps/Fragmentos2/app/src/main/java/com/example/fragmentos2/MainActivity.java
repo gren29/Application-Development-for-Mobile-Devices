57f51aecc06d9b8afd0f75a6acdb2838c6b2e537
RSR <gren2959@gmail.com>
2020-11-24 10:28:11 -0600

    Fragmentos 2

--- Apps/Fragmentos2/app/src/main/java/com/example/fragmentos2/MainActivity.java
+++ Apps/Fragmentos2/app/src/main/java/com/example/fragmentos2/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.fragmentos2;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements MiFragmento.FragmentoListener {
+    TextView    xtv;
+    Button      jbn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
         setContentView(R.layout.activity_main);
+        xtv = (TextView) findViewById(R.id.xtv);
+        jbn = (Button) findViewById(R.id.xbn);
+        jbn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                Fragment f = fm.findFragmentByTag("editor");
+                if (null == f) {
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.add(R.id.xfl, new MiFragmento(), "editor");
+                    ft.commit();
+                }
+                xtv.setText("");
+                Toast.makeText(MainActivity.this, "Utilizando Fragment",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    @Override
+    public void digitado(int r, String s) {
+        TextView jtv = (TextView) findViewById(R.id.xtv);
+        if (r == MiFragmento.OK) {
+            jtv.setText(s);
+        }
+        FragmentManager fm = getFragmentManager();
+        Fragment fe = fm.findFragmentByTag("editor");
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.commit();
+        ft.remove(fe);
     }
 }
