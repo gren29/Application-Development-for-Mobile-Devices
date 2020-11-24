c6aa008c3401c7d96c0358ff25febeee5483e46f
RSR <gren2959@gmail.com>
2020-11-24 10:57:10 -0600

    Fragmentos 3

---
+++ Apps/Fragmentos3/app/src/main/java/com/example/fragmentos3/Grupo.java
@@ -0,0 +1,16 @@
+package com.example.fragmentos3;
+
+public class Grupo{
+    private String de, asunto,texto;
+    public Grupo(String de, String as, String te){
+        this.de = de; this.asunto = as; this.texto = te;
+    }
+    public String getDe(){
+        return de; }
+    public String getAsunto(){
+        return asunto;
+    }
+    public String getTexto(){
+        return texto;
+    }
+}
--- Apps/Fragmentos3/app/src/main/java/com/example/fragmentos3/MainActivity.java
+++ Apps/Fragmentos3/app/src/main/java/com/example/fragmentos3/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.fragmentos3;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity implements FragmentListado.GruposListener {
+    FragmentListado fl;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
         setContentView(R.layout.activity_main);
+        fl = (FragmentListado)getSupportFragmentManager().findFragmentById(R.id.FrgListado);
+        fl.setGruposListener(this);
+    }
+    @Override
+    public void onGrupoSeleccionado(Grupo c) {
+        boolean bo = (getSupportFragmentManager().findFragmentById(R.id.FrgDetalle) != null);
+        if(bo) {
+            ((FragmentDetalle) getSupportFragmentManager().findFragmentById( R.id.FrgDetalle) ).mostrarDetalle( c.getTexto() );
+        }
+        else {
+            Intent i = new Intent(this, DetalleActivity.class);
+            i.putExtra(DetalleActivity.EXTRA_TEXTO, c.getTexto());
+            startActivity(i);
+        }
     }
 }
