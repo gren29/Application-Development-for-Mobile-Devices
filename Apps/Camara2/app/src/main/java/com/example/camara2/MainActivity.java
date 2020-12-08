e2408d0f0739ff9af0f4db0b902aec5cd1247b56
RSR <gren2959@gmail.com>
2020-12-07 19:38:04 -0600

    Camara 2

--- Apps/Camara2/app/src/main/java/com/example/camara2/MainActivity.java
+++ Apps/Camara2/app/src/main/java/com/example/camara2/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.camara2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import java.io.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.*;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
+import android.os.*;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
+public class MainActivity extends Activity {
+    Button      jbn;
+    RadioButton jrb1, jrb2;
+    Intent i;
+    int TAKE_PICTURE = 1;
+    int SELECT_PICTURE = 2;
+    String s = "";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle b){
+        super.onCreate(b);
         setContentView(R.layout.activity_main);
+        s = Environment.getExternalStorageDirectory() + "/test.jpg";
+        jbn = (Button)findViewById(R.id.xbn1);
+        jbn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jrb1 = (RadioButton)findViewById(R.id.xrb1);
+                jrb2 = (RadioButton)findViewById(R.id.xrb2);
+                i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                int code = TAKE_PICTURE;
+                if (jrb1.isChecked()) {
+                    File f1 = new File((s));
+                    Uri output = Uri.parse(f1.toString());
+                    i.putExtra(MediaStore.EXTRA_OUTPUT, output);
+                } else if (jrb2.isChecked()){
+                    i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    code = SELECT_PICTURE;
+                }
+                startActivityForResult(i, code);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_PICTURE) {
+            if (data != null) {
+                if (data.hasExtra("data")) {
+                    ImageView iv = (ImageView)findViewById(R.id.xiv1);
+                    iv.setImageBitmap((Bitmap)data.getParcelableExtra("data"));
+                }
+            } else {
+                ImageView iv = (ImageView)findViewById(R.id.xiv1);
+                iv.setImageBitmap(BitmapFactory.decodeFile(s));
+                new MediaScannerConnectionClient() {
+                    private MediaScannerConnection msc = null; {
+                        msc = new MediaScannerConnection(getApplicationContext(), this);
+                    }
+                    public void onMediaScannerConnected() {
+                        msc.scanFile(s, null);
+                    }
+                    public void onScanCompleted(String path, Uri uri) {
+                        msc.disconnect();
+                    }
+                };
+            }
+        }else if(requestCode == SELECT_PICTURE){
+            Uri image = data.getData();
+            InputStream is;
+            try {
+                is = getContentResolver().openInputStream(image);
+                BufferedInputStream bis = new BufferedInputStream(is);
+                Bitmap bitmap = BitmapFactory.decodeStream(bis);
+                ImageView iv = (ImageView)findViewById(R.id.xiv1);
+                iv.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {}
+        }
     }
 }
